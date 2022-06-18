7efba278937ea0402cf002f60a3acc88e0a3a35c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-06-18 18:30:07 +0200

    feat: hold die selection and add instructions

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,36 +5,82 @@ import { nanoid } from 'nanoid';
 
 function App() {
 /**
- * Challenge: Create a `Roll Dice` button that will re-roll
- * all 10 dice
+ * Challenge: Update the `rollDice` function to not just roll
+ * all new dice, but instead to look through the existing dice
+ * to NOT role any that are being `held`.
  * 
- * Clicking the button should generate a new array of numbers
- * and set the `dice` state to that new array (thus re-rendering
- * the array to the page)
+ * Hint: this will look relatively similiar to the `holdDice`
+ * function below. When creating new dice, remember to use
+ * `id: nanoid()` so any new dice have an `id` as well.
  */
+
   const [dice, setDice] = useState(allNewDice());
 
   function allNewDice(){
     var newDiceArray = [];
     for(var i = 0; i < 10; i++) {
-      newDiceArray.push(Math.floor(Math.random() * (6 - 1 + 1) + 1));
+      newDiceArray.push(
+        createNewDie()
+      );
     }
     return newDiceArray;
   }
 
-  const diceElements = dice.map(die => <Die key={nanoid()} value={die}/>);
-
-  function rollDice(){
-    setDice(allNewDice());
+  function createNewDie(){
+    return {
+      id: nanoid(),
+      isHeld: false,
+      number: Math.floor(Math.random() * (6 - 1 + 1) + 1)
+    };
   }
+
+  function updateDice(){
+    setDice(prevDice => {
+      return prevDice.map(die => {
+        if(die.isHeld) {
+          return die;
+        } else {
+          return createNewDie();
+        }
+      })
+    })
+  }
+
+  function onDieClickHandler(evt, id){
+    console.log({evt, id});
+    setDice(dicePrevState => {
+      return dicePrevState.map(die => {
+        if(die.id === id) {
+          return {
+            ...die,
+            isHeld: !die.isHeld
+          }
+        } else {
+          return die;
+        }
+      });
+    });
+  }
+
+  const diceElements = dice.map(die => {     
+    return <Die 
+      key={die.id}
+      id={die.id} 
+      value={die.number} 
+      isHeld={die.isHeld}
+      onDieClickHandler={onDieClickHandler}
+    />
+  });
 
   return (
     <div className="app">
       <main>
+        <h1 className="app__title">Tenzies</h1>
+        <p className="app__instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className="dices">
           {diceElements}
         </div>
-        <button className="rollButton" onClick={rollDice}>Roll</button>
+        <button className="rollButton" onClick={updateDice}>Roll</button>
       </main>
     </div>
   );
--- src/components/die/Die.jsx
+++ src/components/die/Die.jsx
@@ -1,8 +1,12 @@
 import "./Die.scss";
 
 function Die(props) {
+  const isHeldClassName = props.isHeld ? "die--held" : "";
   return (
-    <div className="die">
+    <div 
+      className={`die ${isHeldClassName}`}
+      onClick={(evt) => props.onDieClickHandler(evt, props.id)}
+    >
       <h2 className="die__number">{props.value}</h2>
     </div>
   )
