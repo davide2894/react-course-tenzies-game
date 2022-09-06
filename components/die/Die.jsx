ff8ef3e0ccc40a9ab667b8ae0ec7ee8939a8707b
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2022-09-06 13:39:49 +0200

    Updates

---
+++ components/die/Die.jsx
@@ -0,0 +1,17 @@
+import { useId } from "react";
+import "./Die.scss";
+
+function Die(props) {
+  const isHeldClassName = props.isHeld ? "die--held" : "";
+  return (
+    <div 
+      id={useId()}
+      className={`die ${isHeldClassName}`}
+      onClick={(evt) => props.onDieClickHandler(evt, props.id, props.number)}
+    >
+      <h2 className="die__number">{props.number}</h2>
+    </div>
+  )
+}
+
+export default Die
